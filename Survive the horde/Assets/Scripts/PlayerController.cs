2954dd5f3ed07cab997f8c44bba297b8d1cba145
JustinHull <justin.hull@hgs.hiddengeniusproject.org>
2023-07-18 14:14:36 -0400

    dfrtgyhuj

--- Survive the horde/Assets/Scripts/PlayerController.cs
+++ Survive the horde/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,12 @@ public class PlayerController : MonoBehaviour
     private float forwardInput;
     public float speed;
     public float turnSpeed;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,5 +26,28 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the player
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        // Plays forwards animation by setting "isForwards" to true when you press w
+        if (Input.GetKey("w"))
+        {
+            animator.SetBool("isForwards", true);
+        }
+        // Goes back to idle by setting "isForwards" to false when you are not pressing w
+        if(!Input.GetKey("w"))
+        {
+            animator.SetBool("isForwards", false);
+        }
+
+        // Plays Backwards animation by setting "isBackwards" to true when you press s
+        if (Input.GetKey("s"))
+        {
+            animator.SetBool("isBackwards", true);
+        }
+        // Goes back to idle by setting "isBackwards" to false when you are not pressing s
+        if (!Input.GetKey("s"))
+        {
+            animator.SetBool("isBackwards", false);
+        }
+       
     }
 }
